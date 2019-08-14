1f3da7072a42b4126061c4025d20291bb1288511
PatrickGarcia1 <53844521+PatrickGarcia1@users.noreply.github.com>
2019-08-13 21:45:33 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ var last = prompt("What is your last name?");
 
 var age = prompt("What is your age?");
 
-console.log("My name is", first, last + ".");
+console.log("My name is" + " " +  first + " "+  last + ".");
 
-console.log("I am" , age, "years old.");
+console.log("I am" + " " + age + " " +  "years old.");
