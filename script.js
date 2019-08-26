ba2b0252d3875ee2ffc27a15bd66c64b5d113b1a
PatrickGarcia1 <53844521+PatrickGarcia1@users.noreply.github.com>
2019-08-26 19:50:40 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var first = prompt("What is your first name?");
-
-var last = prompt("What is your last name?");
-
-var age = prompt("What is your age?");
-
-console.log("My name is" + " " +  first + " "+  last + ".");
-
-console.log("I am" + " " + age + " " +  "years old.");
